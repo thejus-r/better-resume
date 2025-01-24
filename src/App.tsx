c749089944061533eca2c70f80c6a5e2b1321b57
Thejus Rajendran <thejusr1999@icloud.com>
2025-01-24 22:53:00 +0530

    feat: pdf render with webworker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Container from "@components/Container/container";
+import { PDFViewer } from "@components/PDFViewer";
+import { usePDF } from "@react-pdf/renderer";
 import { Section } from "@components/Section";
+import { MyDocument } from "@lib/renderer";
+
 function App() {
+  const [instance, setInstance] = usePDF({ document: MyDocument });
   return (
     <div className="flex h-screen w-full">
       <Container>
         <Section />
       </Container>
       <div className="flex h-full w-1/2 items-center justify-center bg-gray-300">
-        Resume Live Preview
+        {instance.loading ? <p>Loading</p> : <PDFViewer src={instance.url!} />}
       </div>
     </div>
   );
