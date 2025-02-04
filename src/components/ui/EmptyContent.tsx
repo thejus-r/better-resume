590c3902313a62c0f828c8b0ecf1a58212d74a41
Thejus Rajendran <thejusr1999@icloud.com>
2025-02-04 23:10:47 +0530

    component: EmptyContentState

---
+++ src/components/ui/EmptyContent.tsx
@@ -0,0 +1,7 @@
+const EmptyContent = () => {
+  return <div className="w-full text-xs text-gray-400 font-mono h-20 flex items-center justify-center bg-gray-50 border border-dashed border-gray-300 rounded-xl">
+    It looks pretty empty!
+  </div>
+}
+
+export { EmptyContent }
