9a009d24baa069d931ee366b94c3693a592b07c8
Thejus Rajendran <thejusr1999@gmail.com>
2025-02-26 22:20:33 +0530

    utils: datefns

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,1 +1,10 @@
-const date = Date.parse("04 Dec 1995")
+function convertDateToString(date: Date): string {
+  const fullISOString = date.toISOString()
+  return fullISOString.slice(0, 7)
+}
+
+function convertStringToDate(str: string): Date {
+  return new Date(str)
+}
+
+export { convertDateToString, convertStringToDate }
