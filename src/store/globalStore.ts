029435ad04e77dce9963dcacf081ae014e141d4d
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-15 00:13:14 +0530

    feat: added redux persist and saving state to local storage

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -1,17 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import skillsReducer from "../features/skills/skillsSlice";
 import projectsReducer from "../features/project/projectSlice";
 import personalReducer from "../features/personal/slice";
 
-const store = configureStore({
-  reducer: {
-    skills: skillsReducer,
-    projects: projectsReducer,
-    personal: personalReducer,
-  },
+const rootReducer = combineReducers({
+  personal: personalReducer,
+  projects: projectsReducer,
+  skills: skillsReducer,
 });
+
+const presistConfig = {
+  key: "root",
+  storage,
+};
+
+const presistedState = persistReducer(presistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: presistedState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
