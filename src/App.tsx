60947c3a0a5575a98983ada349ac9d45b15b0b95
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-16 01:25:42 +0530

    bug: build error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { MasonryLayout } from "@components/Masonry";
-import { PDF } from "./features/pdf/ResumePDF";
 import { FloatBar } from "./features/pdf/FloatBar";
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-100 p-4 md:px-24">
-      {/* <PDF /> */}
       <MasonryLayout />
       <FloatBar />
     </div>
